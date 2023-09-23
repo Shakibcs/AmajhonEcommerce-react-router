f1f5bc7a81bad5e10b012be33780cee6caabd333
Shakibul Alam <droid@SHAKIBULS-AIR.station>
2023-09-23 16:30:56 +0100

    use flowbite ecommerce card for product

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,23 +1,26 @@
 import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import ProductsCard from "./ProductsCard";
 
 const Products = () => {
 
-    // const {products} = useLoaderData()
-    // console.log(products);
+    const {products} = useLoaderData()
+    console.log(products);
 
-    const [products,setProducts] = useState([])
-    useEffect(()=>{
-        fetch('https://dummyjson.com/products')
-        .then(res => res.json())
-        .then(data=>setProducts(data.products))
-    },[])
+    // const [products,setProducts] = useState([])
+    // useEffect(()=>{
+    //     fetch('https://dummyjson.com/products')
+    //     .then(res => res.json())
+    //     .then(data=>setProducts(data.products))
+    // },[])
 
     console.log(products)
 
     return (
-        <div>
-            <div>Products page</div>
+        <div className="grid grid-cols-3 gap-5 p-5">
+            {
+                products.map(product => <ProductsCard key={product.id} product={product}></ProductsCard> )
+            }
         </div>
     );
 };
