c5710f2b84a7b06b38b65c90613245f76117c5ee
Shakibul Alam <droid@SHAKIBULS-AIR.station>
2023-09-23 16:05:09 +0100

    fetch data with loader and useLoaderData() hook

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,4 +1,20 @@
+import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+
 const Products = () => {
+
+    // const {products} = useLoaderData()
+    // console.log(products);
+
+    const [products,setProducts] = useState([])
+    useEffect(()=>{
+        fetch('https://dummyjson.com/products')
+        .then(res => res.json())
+        .then(data=>setProducts(data.products))
+    },[])
+
+    console.log(products)
+
     return (
         <div>
             <div>Products page</div>
